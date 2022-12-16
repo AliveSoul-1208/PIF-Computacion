c7dee48e81b17992349452ae95679c96a585ba34
Brayanparicahuachoque <120491737+Brayanparicahuachoque@users.noreply.github.com>
2022-12-16 16:53:57 -0500

    gg

--- Trabajo.py
+++ Trabajo.py
@@ -700,11 +700,10 @@ st.write("# ![linea 2](https://user-images.githubusercontent.com/19308295/115926
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
 with st.expander("See explanation"):
-    st.write(\"\"\"
-        The chart above shows some numbers I picked for you.
+    st.write("""###The chart above shows some numbers I picked for you.
         I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    \"\"\")
+        be random.""")
+   
     st.image("https://static.streamlit.io/examples/dice.jpg")
 
 

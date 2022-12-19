6e0afffde26b2d8e8d6ef66eb2e75d6414fa49ea
joselo17 <120488979+joselo17@users.noreply.github.com>
2022-12-18 20:37:43 -0500

    verdes

--- Trabajo2.py
+++ Trabajo2.py
@@ -17,9 +17,9 @@ with st.sidebar:
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#ffffff"},
-        "icon": {"color": "Purple", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#8B008B"},
-        "nav-link-selected": {"background-color": "#40E0D0"},
+        "icon": {"color": "Green", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#7CFC00"},
+        "nav-link-selected": {"background-color": "#000000"},
     }
     )
 
